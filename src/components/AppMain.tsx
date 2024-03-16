2948e9039f5121eb8fa83678045e008841a67c88
Piotr Brzozowski <pbsrv@icloud.com>
2024-03-16 22:20:24 +0100

    Added usage of fetch logic to the subBtn

--- src/components/AppMain.tsx
+++ src/components/AppMain.tsx
@@ -1,6 +1,25 @@
-import React from "react";
+import { useState } from "react";
+import { fetchWeatherData } from "../services/fetch";
 
 export const AppMain = () => {
+  const [placeValue, setPlaceValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const HandleWeatherFetch = (place: string) => {
+    setIsLoading(true); // Set loading to true before fetching
+
+    //using fetch logic to get weather data
+    fetchWeatherData(place)
+      .then((data) => {
+        console.log("Fetched data succesfully", data);
+        setIsLoading(false); // Set loading to false after fetching
+      })
+      .catch((error) => {
+        console.error(error.message);
+        setIsLoading(false);
+      });
+  };
+
   return (
     <div className="App">
       <div className="container">
@@ -8,9 +27,22 @@ export const AppMain = () => {
         <input
           className="searchBox"
           type="text"
+          value={placeValue}
+          onChange={(e) => setPlaceValue(e.target.value)}
           placeholder="Enter a town to check the weather..."
         />
-        <button>Check the weather</button>
+        {isLoading ? (
+          <button className="loadingButton" disabled={true}>
+            Checking...
+          </button>
+        ) : (
+          <button
+            className="submitButton"
+            onClick={() => HandleWeatherFetch(placeValue)}
+          >
+            Check the weather
+          </button>
+        )}
       </div>
     </div>
   );
--- src/services/fetch.tsx
+++ src/services/fetch.tsx
@@ -1,39 +1,17 @@
-import { useState, useEffect } from "react";
-import { apiKey } from "../../main";
-const place = "Warsaw";
-const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${place}&aqi=no`;
+import { apiKey } from "../main";
 
-export const Fetch = () => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+export const fetchWeatherData = async (place: string) => {
+  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${place}&aqi=no`;
 
-  // fetching data from weatherapi.com
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        //setting loading to true
-        setLoading(true);
-        //fetching
-        const response = await fetch(url);
-        // if response get 4xx error then throw error
-        if (!response.ok) {
-          throw new Error(`HTTP error accured ${response.status}`);
-        }
-        //converting data to json
-        const weatherData = await response.json();
-        setData(weatherData);
-        console.log(weatherData);
-        setError(null);
-      } catch (error) {
-        setError(error.message);
-        setData(null);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getData();
-  }, []);
-
-  return <div>fetch</div>;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error occurred ${response.status}`);
+    }
+    const weatherData = await response.json();
+    return weatherData;
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
 };
