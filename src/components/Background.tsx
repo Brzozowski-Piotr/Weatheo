7f19efaaa4bad294259f965e7f29349f881717e8
Piotr Brzozowski <pbsrv@icloud.com>
2024-05-03 00:05:39 +0200

    Added dark mode and changed some styling in light mode, more refactor of css.

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -3,6 +3,7 @@ import "../background.css";
 export const Background = () => {
   return (
     <div className="bg">
+      <div className="stars"></div>
       <div className="cloud"></div>
     </div>
   );
